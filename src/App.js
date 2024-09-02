2d6f42855a8bf3a01a458a6005bef30ded6a478e
tmak1 <tarik.khan.git@gmail.com>
2024-09-02 16:48:16 +1000

    docker init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {add} from 'lodash';
 
 function App() {
   console.log('');
@@ -12,7 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+          Learn React {add(2, 3)}
         </a>
       </header>
     </div>
