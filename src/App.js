42ebaf3d3fd980a0a3f605b8137a3562bde432c5
tmak1 <tarik.khan.git@gmail.com>
2024-09-02 17:26:03 +1000

    add package

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {add} from 'lodash';
+import {helloWorld} from 'hello-world-npm';
 
 function App() {
   console.log('');
@@ -13,8 +14,11 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React {add(2, 3)}
+          Learn React
         </a>
+        <p>
+          {add(2, 3)} {helloWorld()}
+        </p>
       </header>
     </div>
   );
